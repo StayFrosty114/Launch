f1ac945e36366b151e3eb9a2a21fb2935f488a30
Ben Gilbert <bengilbert132@gmail.com>
2020-01-13 14:57:51 -0800

    Initial Commit

---
+++ Assets/Scripts/ObjectPooling.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooling : MonoBehaviour
+{
+    public List<GameObject> chunks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnChunk();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnChunk()
+    {
+
+    }
+
+    public void GetChunk()
+    {
+        Debug.Log("Retrieve");
+    }
+}
