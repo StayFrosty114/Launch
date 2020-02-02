d4f4a43c71a651266fc99ed0dc625e31f3719086
Ben Gilbert <bengilbert132@gmail.com>
2020-02-02 12:07:46 -0800

    - Added game music - Edited cannon shot sound - Staggered object pool

--- Assets/Scripts/ObjectPooling.cs
+++ Assets/Scripts/ObjectPooling.cs
@@ -11,10 +11,14 @@ public class ObjectPooling : MonoBehaviour
     public List<GameObject> inactiveChunks;
     private GameObject chunkToPlace;
     public GameObject spawnPoint;
+
+    private float currentTime;
+
     
     // Start is called before the first frame update.
     void Start()
     {
+        currentTime = 0;
         overlord = GetComponent<Overlord>();
 
         foreach (GameObject chunk in chunks)
@@ -23,6 +27,11 @@ public class ObjectPooling : MonoBehaviour
             chunkToPlace.SetActive(false);
             inactiveChunks.Add(chunkToPlace);
         }
+    }
+
+    private void Update()
+    {
+        currentTime = Time.deltaTime * 1;
     }
 
     // Grabs a chunk from the inactive list and places it at the top.
@@ -40,15 +49,42 @@ public class ObjectPooling : MonoBehaviour
     // Finds the correct type of chunk in the inactive list.
     private GameObject ChooseChunk()
     {
-        GameObject newChunk = chunks[Random.Range(0, chunks.Count)];
-        foreach (GameObject chunk in inactiveChunks)
+        if (currentTime <= 30)
         {
-            if(chunk.name.Contains(newChunk.name))
+            GameObject newChunk = chunks[Random.Range(0, 5)];
+            foreach (GameObject chunk in inactiveChunks)
             {
-                return chunk;
+                if (chunk.name.Contains(newChunk.name))
+                {
+                    return chunk;
+                }
             }
+            return Instantiate(newChunk, spawnPoint.transform.position, spawnPoint.transform.rotation, overlord.transform);
         }
-        return Instantiate(newChunk, spawnPoint.transform.position, spawnPoint.transform.rotation, overlord.transform);
+        else if (currentTime >= 30 && currentTime <= 60)
+        {
+            GameObject newChunk = chunks[Random.Range(0, chunks.Count)];
+            foreach (GameObject chunk in inactiveChunks)
+            {
+                if(chunk.name.Contains(newChunk.name))
+                {
+                    return chunk;
+                }
+            }
+            return Instantiate(newChunk, spawnPoint.transform.position, spawnPoint.transform.rotation, overlord.transform);
+        }
+        else
+        {
+            GameObject newChunk = chunks[Random.Range(2, chunks.Count)];
+            foreach (GameObject chunk in inactiveChunks)
+            {
+                if (chunk.name.Contains(newChunk.name))
+                {
+                    return chunk;
+                }
+            }
+            return Instantiate(newChunk, spawnPoint.transform.position, spawnPoint.transform.rotation, overlord.transform);
+        }
     }
 
     // Deactivates a chunk that hits the bottom trigger and adds it to the inactive list.
